6ffa5fd0c0cde11bb95a26f523f34c0c7685f059
ejurgensen <espenjurgensen@gmail.com>
2023-10-21 17:08:01 +0200

    Websrc 5

--- web-src/src/main.js
+++ web-src/src/main.js
@@ -11,7 +11,7 @@ import VueProgressBar from '@aacassandra/vue3-progressbar'
 import { router } from './router'
 import store from './store'
 
-// Mock change 4
+// Mock change 5
 
 const app = createApp(App)
   .use(store)
